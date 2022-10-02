2815c89061abe3e4d7ef753ea94656bb3f86eba8
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-10-02 13:27:13 +0300

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+    def show
+        user = find_user
+        render json: user, include: :items
+    end
+
+    private
+
+    def find_user
+        user = User.find(params[:id])
+    end
+
 end
